80dadcdf4969387c9dda17c6294ddc56d28f2189
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2023-06-22 18:31:44 -0400

    update test case

--- examples/run_ace_generate.sh
+++ examples/run_ace_generate.sh
@@ -1,8 +1,8 @@
 ace generate \
   --num-peptides 100 \
   --num-peptides-per-pool 10 \
-  --num-coverage 2 \
+  --num-coverage 3 \
   --num-processes 4 \
-  --output-csv-file 100peptides_10perpool_2x.csv \
+  --output-csv-file 100peptides_10perpool_3x.csv \
   --assign-well-ids 1 \
   --plate-type 96-well_plate
